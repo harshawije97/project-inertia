5909a24259893c69465dccccf518f59a304fce34
harshawije97 <harshawije99@gmail.com>
2026-06-14 00:08:47 +0530

    migrate models

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Feature extends Model
 {
-    //
+    public function upvotes(): HasMany
+    {
+        return $this->hasMany(Upvote::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comments::class);
+    }
+    // This model owns a foreign key that belongs to User
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2026_06_13_165519_create_features_table.php
+++ database/migrations/2026_06_13_165519_create_features_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
             $table->timestamps();
         });
     }
